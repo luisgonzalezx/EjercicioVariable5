3b8d5fa1fb340c683e0d577a8b253d954e0bb179
luis <lagonzalez1688@gmail.com>
2020-10-13 00:44:05 -0700

    Ejercicio variable 5

---
+++ src/ejerciciovariable5/Ejerciciovariable5.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciovariable5;
+
+
+public class Ejerciciovariable5 {
+
+ public static void main(String[] args) {
+        
+     
+        int b=1;
+        if(b>=0)
+            
+            System.out.print("el número es positivo" );
+        
+        else
+            System.out.print("el número es negativo");
+        
+    }
+    
+}
